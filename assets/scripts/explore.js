17d24015edd1c126e73be3b237db4414f4a81fee
Angelsofttoy <yiren@ucsd.edu>
2024-05-07 15:36:28 -0700

    made change on functionality up till explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,39 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  let talk = new SpeechSynthesisUtterance();
+  let voiceSelect = document.querySelector("#voice-select");
+  var icon = document.querySelector('img');
+
+  window.speechSynthesis.onvoiceschanged = () => {
+    const voices = speechSynthesis.getVoices();
+    talk.voice = voices[0];
+
+    voices.forEach((voice) =>{
+      voiceSelect.options.add(new Option(voice.name, voice.name));
+    });
+  };
+  
+  voiceSelect.addEventListener("change", () =>{
+    const voices = speechSynthesis.getVoices();
+    talk.voice = voices.find(voice => voice.name === voiceSelect.value);
+  });
+
+  document.querySelector("button").addEventListener("click",() => {
+    talk.text = document.querySelector("textarea").value;
+    window.speechSynthesis.speak(talk);
+
+  // Update the icon
+    if (speechSynthesis.speaking) {
+        icon.src = 'assets/images/smiling-open.png';
+        
+    }
+    talk.addEventListener('end', () => {
+      // Update the icon to closed mouthed
+      icon.src = 'assets/images/smiling.png';
+    });
+  });
+ 
+  
+  
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,80 @@
 // expose.js
-
 window.addEventListener('DOMContentLoaded', init);
-
 function init() {
-  // TODO
+  
+  const element = document.querySelector('#horn-select');
+  var icon = document.querySelector('img');
+  const buttons = document.querySelectorAll("button");
+  const audioAir = new Audio('assets/audio/air-horn.mp3');
+  const audioCar = new Audio('assets/audio/car-horn.mp3');
+  const audioParty = new Audio('assets/audio/party-horn.mp3');
+  const volumeSlider = document.querySelector('#volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+  
+  const jsConfetti = new JSConfetti();
+
+
+  element.addEventListener('change', () => {
+    if (element.value == "air-horn") {
+      icon.src = 'assets/images/air-horn.svg';
+    }
+    if (element.value == "car-horn") {
+      icon.src = 'assets/images/car-horn.svg';
+    }
+    if (element.value == "party-horn") {
+      icon.src = 'assets/images/party-horn.svg';
+    }
+  });
+
+//play sound based on value of selected element
+  
+  buttons.forEach(button => {
+    button.addEventListener("click", () =>
+    {
+      if (element.value == "air-horn") {
+        audioAir.play();
+      }
+      if (element.value == "car-horn") {
+        audioCar.play();
+      }
+      if (element.value == "party-horn") {
+        audioParty.play();
+        jsConfetti.addConfetti()
+      }
+    });
+  });
+
+  function updateVolumeIcon(volume){
+    volume = parseFloat(volume);
+    if(volume === 0){
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+    }
+    else if(volume < 33){
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+    }
+    else if( volume < 67){
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+    }
+    else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+    }
+  }
+
+  updateVolumeIcon(volumeSlider.value);
+
+  volumeSlider.addEventListener('input', () =>{
+    const volume = volumeSlider.value;
+    updateVolumeIcon(volume);
+
+    audioAir.volume = volume/100;
+    audioCar.volume = volume/100;
+    audioParty.volume = volume/100;
+  });
+
+  updateVolumeIcon(volumeSlider.value);
+
+  
+
+
+
 }
